c32f2605005d30308d9b39dd7143786b6ab1dac7
eldarmoore <eldar.moore@gmail.com>
2024-08-04 22:18:47 +0300

    Add database connection and fetch products
    This commit establishes a connection to the MySQL database and fetches all products from the 'product' table. The fetched data is then printed to the screen for debugging purposes.

--- src/index.php
+++ src/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Welcome to PHP MVC 2024';
+$dsn = "mysql:host=db;dbname=php-mvc-2024;charset=utf8;port=3306";
+
+$pdo = new PDO($dsn, 'php_mvc_2024', 'php_mvc_2024', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$stmt = $pdo->query('SELECT * FROM product');
+
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);
